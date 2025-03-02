d17310ebbdb7b6e2dc69b177a5365d99e22bea0f
Talkar <2336860+Talkar@users.noreply.github.com>
2025-03-02 10:32:08 +0100

    Customize appearance

--- config/appearance.lua
+++ config/appearance.lua
@@ -4,57 +4,69 @@ local color = Utils.fn.color
 ---@diagnostic disable-next-line: undefined-field
 local G = require("wezterm").GLOBAL
 
-local Config = {}
+local config = {}
 
-Config.color_schemes = color.get_schemes()
-Config.color_scheme = color.get_scheme()
+config.color_schemes = color.get_schemes()
+config.color_scheme = color.get_scheme()
 
-local theme = Config.color_schemes[Config.color_scheme]
+local theme = config.color_schemes[config.color_scheme]
 
-Config.background = {
+-- Set background image
+config.background = {
   {
-    source = { Color = theme.background },
-    width = "100%",
-    height = "100%",
-    opacity = G.opacity or 1,
+    source = {
+      File = '/home/talkar/Pictures/coruscant-square.png'
+    },
+  },
+  {
+    source = {
+      Color =  "rgba(25, 25, 44, 0.95)"
+    },
+    height = '100%',
+    width = '100%'
   },
 }
 
-Config.bold_brightens_ansi_colors = "BrightAndBold"
+-- Set initial size 
+config.initial_cols = 200
+config.initial_rows = 50
+
+
+config.bold_brightens_ansi_colors = "BrightAndBold"
 
 ---char select and command palette
-Config.char_select_bg_color = theme.brights[6]
-Config.char_select_fg_color = theme.background
-Config.char_select_font_size = 12
+config.char_select_bg_color = theme.brights[6]
+config.char_select_fg_color = theme.background
+config.char_select_font_size = 12
 
-Config.command_palette_bg_color = theme.brights[6]
-Config.command_palette_fg_color = theme.background
-Config.command_palette_font_size = 14
-Config.command_palette_rows = 20
+config.command_palette_bg_color = theme.brights[6]
+config.command_palette_fg_color = theme.background
+config.command_palette_font_size = 14
+config.command_palette_rows = 20
 
 ---cursor
-Config.cursor_blink_ease_in = "EaseIn"
-Config.cursor_blink_ease_out = "EaseOut"
-Config.cursor_blink_rate = 500
-Config.default_cursor_style = "BlinkingUnderline"
-Config.cursor_thickness = 1
-Config.force_reverse_video_cursor = true
+config.cursor_blink_ease_in = "EaseIn"
+config.cursor_blink_ease_out = "EaseOut"
+config.cursor_blink_rate = 500
+config.default_cursor_style = "BlinkingUnderline"
+config.cursor_thickness = 1
+config.force_reverse_video_cursor = true
 
-Config.enable_scroll_bar = true
+config.enable_scroll_bar = true
 
-Config.hide_mouse_cursor_when_typing = true
+config.hide_mouse_cursor_when_typing = true
 
 ---text blink
-Config.text_blink_ease_in = "EaseIn"
-Config.text_blink_ease_out = "EaseOut"
-Config.text_blink_rapid_ease_in = "Linear"
-Config.text_blink_rapid_ease_out = "Linear"
-Config.text_blink_rate = 500
-Config.text_blink_rate_rapid = 250
+config.text_blink_ease_in = "EaseIn"
+config.text_blink_ease_out = "EaseOut"
+config.text_blink_rapid_ease_in = "Linear"
+config.text_blink_rapid_ease_out = "Linear"
+config.text_blink_rate = 500
+config.text_blink_rate_rapid = 250
 
 ---visual bell
-Config.audible_bell = "SystemBeep"
-Config.visual_bell = {
+config.audible_bell = "SystemBeep"
+config.visual_bell = {
   fade_in_function = "EaseOut",
   fade_in_duration_ms = 200,
   fade_out_function = "EaseIn",
@@ -62,16 +74,16 @@ Config.visual_bell = {
 }
 
 ---window appearance
-Config.window_padding = { left = 2, right = 2, top = 2, bottom = 1 }
-Config.integrated_title_button_alignment = "Right"
-Config.integrated_title_button_style = "Windows"
-Config.integrated_title_buttons = { "Hide", "Maximize", "Close" }
+config.window_padding = { left = 2, right = 2, top = 2, bottom = 1 }
+config.integrated_title_button_alignment = "Right"
+config.integrated_title_button_style = "Windows"
+config.integrated_title_buttons = { "Hide", "Maximize", "Close" }
 
 ---exit behavior
-Config.clean_exit_codes = { 130 }
-Config.exit_behavior = "CloseOnCleanExit"
-Config.exit_behavior_messaging = "Verbose"
-Config.skip_close_confirmation_for_processes_named = {
+config.clean_exit_codes = { 130 }
+config.exit_behavior = "CloseOnCleanExit"
+config.exit_behavior_messaging = "Verbose"
+config.skip_close_confirmation_for_processes_named = {
   "bash",
   "sh",
   "zsh",
@@ -82,8 +94,8 @@ Config.skip_close_confirmation_for_processes_named = {
   "pwsh.exe",
   "powershell.exe",
 }
-Config.window_close_confirmation = "AlwaysPrompt"
+config.window_close_confirmation = "AlwaysPrompt"
 
-color.set_tab_button(Config, theme)
+color.set_tab_button(config, theme)
 
-return Config
+return config
